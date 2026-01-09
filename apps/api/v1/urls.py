633d3b10fa46a990e176de5e9dce15d81907d18e
NazarHumen <nazargumen172@gmail.com>
2026-01-09 10:00:04 +0200

    feat(api): add users routes to v1 api

--- apps/api/v1/urls.py
+++ apps/api/v1/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("catalog/", include("apps.goods.urls", namespace="catalog")),
     path("cart/", include("apps.carts.urls", namespace="cart")),
     path("orders/", include("apps.orders.urls", namespace="orders")),
+    path("users/", include("apps.users.urls")),
 ]
