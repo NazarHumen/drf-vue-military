73917884dd08aed918b9237e8042fe329a2e7c61
NazarHumen <nazargumen172@gmail.com>
2026-01-14 22:12:42 +0200

    chore(tools): increase default page_size from 3 to 6

--- apps/tools/pagination.py
+++ apps/tools/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 6
     page_size_query_param = "page_size"
     max_page_size = 100
 
