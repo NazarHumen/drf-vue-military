7c12b9c30d796b620126466176e26d5438bc626a
NazarHumen <nazargumen172@gmail.com>
2026-01-14 22:13:02 +0200

    feat(goods): implement update_exchange_rate management command

--- apps/goods/management/commands/update_exchange_rate.py
+++ apps/goods/management/commands/update_exchange_rate.py
@@ -1,9 +1,30 @@
-# from django.core.management.base import BaseCommand
-# from goods.models import ExchangeRate
-#
-# class Command(BaseCommand):
-#     help = 'Оновлює курс валют'
-#
-#     def handle(self, *args, **kwargs):
-#         ExchangeRate.get_exchange_rate('USD', 'UAH')
-#         self.stdout.write(self.style.SUCCESS("Курс валют оновлено"))
+from django.core.management.base import BaseCommand
+
+from apps.goods.models import Products
+from apps.goods.utils import update_exchange_rate_via_api
+
+
+class Command(BaseCommand):
+    help = "Оновлює курс валют USD -> UAH та перераховує ціни всіх продуктів"
+
+    def handle(self, *args, **kwargs):
+        # Оновлюємо курс валют
+        rate = update_exchange_rate_via_api("USD", "UAH")
+        if rate:
+            self.stdout.write(
+                self.style.SUCCESS(f"Курс валют оновлено: 1 USD = {rate} UAH")
+            )
+
+            # Пересейвлюємо всі продукти для оновлення цін
+            products = Products.objects.all()
+            count = products.count()
+            for product in products:
+                product.save()
+
+            self.stdout.write(
+                self.style.SUCCESS(f"Оновлено ціни для {count} продуктів")
+            )
+        else:
+            self.stdout.write(
+                self.style.ERROR("Не вдалося оновити курс валют")
+            )
