d8ff0e4f93decbb192e0ecc95346e71c21076001
NazarHumen <nazargumen172@gmail.com>
2026-01-30 13:02:10 +0200

    refactor(users): update user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     image = models.ImageField(
         upload_to="users_images", blank=True, null=True, verbose_name="Аватар"
     )
-    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     email = models.EmailField(max_length=100, unique=True)
 
