9ae4013cb11e0f467da86c698b093e373cf3e6c8
NazarHumen <nazargumen172@gmail.com>
2026-01-30 12:59:05 +0200

    feat(api): update API URL configuration

--- apps/api/v1/urls.py
+++ apps/api/v1/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("catalog/", include("apps.goods.urls", namespace="catalog")),
     path("cart/", include("apps.carts.urls", namespace="cart")),
     path("orders/", include("apps.orders.urls", namespace="orders")),
-    path("users/", include("apps.users.urls")),
+    path("users/", include("apps.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
 ]
