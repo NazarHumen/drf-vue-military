3d1bcff695ae801f9c05d29cd688ea4bd6c72ad2
NazarHumen <nazargumen172@gmail.com>
2026-02-01 19:40:10 +0200

    chore(api): add favorites namespace to API v1

--- apps/api/v1/urls.py
+++ apps/api/v1/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path("catalog/", include("apps.goods.urls", namespace="catalog")),
     path("cart/", include("apps.carts.urls", namespace="cart")),
     path("orders/", include("apps.orders.urls", namespace="orders")),
+    path("favorites/", include("apps.favorites.urls", namespace="favorites")),
     path("users/", include("apps.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
 ]
