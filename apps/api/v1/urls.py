2fea9471c4e8fc3efbdba2cdd665517741fe21d9
NazarHumen <nazargumen172@gmail.com>
2026-02-10 08:46:40 +0200

    feat(api): add feedback create endpoint

--- apps/api/v1/urls.py
+++ apps/api/v1/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from main.views import FeedbackCreateAPIView
 
 urlpatterns = [
     path("catalog/", include("apps.goods.urls", namespace="catalog")),
@@ -8,4 +9,5 @@ urlpatterns = [
     path("users/", include("apps.users.urls", namespace="users")),
     path("payments/", include("apps.payments.urls", namespace="payments")),
     path("accounts/", include("allauth.urls")),
+    path("main/feedback/", FeedbackCreateAPIView.as_view(), name="feedback_create"),
 ]
