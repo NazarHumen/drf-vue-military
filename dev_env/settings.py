b28b20b8f2f57bd9b4922ec721aeaa7f07d51d83
NazarHumen <nazargumen172@gmail.com>
2026-08-03 14:22:36 +0300

    fix(settings): update SITE_ID to 3

--- dev_env/settings.py
+++ dev_env/settings.py
@@ -242,7 +242,7 @@ SOCIALACCOUNT_ADAPTER = "apps.tools.adapters.CustomSocialAccountAdapter"
 ACCOUNT_LOGIN_METHOD = {"email"}
 ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 
-SITE_ID = 2
+SITE_ID = 3
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
